c7d715aceb3ed65e527a4911c10b6ea4b731d62e
Selin Ayyildiz <xcadyix@gmail.com>
2022-10-06 13:08:12 -0400

    commit tree 827a6f26422bdd2892529e04d104f5d8eb58b5d1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove()
+const newHeader = document.createElement("h1")
+document.body.append(newHeader)
+newHeader.id = 'victory'
+newHeader.textContent = "Selin is the Champion"
